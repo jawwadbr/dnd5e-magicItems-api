7f641da947cc33fac9bc6dcaa1c6b9c5972371c7
Bradley Jawwad <bradleyjawwad@hotmail.com>
2023-06-28 00:02:29 -0300

    Add Exceptions and Handlers

---
+++ src/main/java/com/jawbr/exception/handler/SourceBookExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.jawbr.exception.handler;
+
+import com.jawbr.exception.SourceBookExceptionNotFound;
+import com.jawbr.exception.errorResponse.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class SourceBookExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleException(SourceBookExceptionNotFound exc) {
+        ErrorResponse error = new ErrorResponse(HttpStatus.NOT_FOUND.value(), exc.getMessage(), System.currentTimeMillis());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+}
